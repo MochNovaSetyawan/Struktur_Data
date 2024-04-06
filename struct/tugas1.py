cac62c8d1f94f1020f2ec0659843d8bad15a8109
root <root@Chrome>
2024-04-07 00:30:40 +0700

    perubahan tanggal 6 april 2024 (Tugas 1)

--- struct/tugas1.py
+++ struct/tugas1.py
@@ -3,3 +3,26 @@
 # gaji per jam lembur = 15 x gajian per jam.
 # gunakan struct untuk membuat program tersebut
 
+class Gaji:
+    def __init__(self):
+        self.jumlahJamKerja = int(input("Masukan jumlah jam kerja: "))
+        self.gajiPerjam = 50000
+
+    def kalkulasiSalary(self):
+
+        if self.jumlahJamKerja <= 7 :
+            gajiNormal = self.gajiPerjam * self.jumlahJamKerja
+            return gajiNormal
+        
+        elif self.jumlahJamKerja >7 :
+            gajiNormal = self.gajiPerjam * 7
+            lemburPerjam = self.jumlahJamKerja - 7
+            gajilembur = 15 * self.gajiPerjam * lemburPerjam
+            return gajilembur + gajiNormal
+
+    def tampilan(self):
+        print(f"Jam kerja: {self.jumlahJamKerja}")
+        print(f"Gaji yang diperoleh: {self.kalkulasiSalary()}")
+
+gajiKaryawan = Gaji()
+gajiKaryawan.tampilan()
